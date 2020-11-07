168c390d2db3c2fe3239c65041307a9817f91408
Virgie Val <virginia.cinemusic.fvf@gmail.com>
2020-11-07 11:42:24 +0700

    week9

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from "body-parser";
 import cors from 'cors';
 import http from 'http';
+import mongoose from 'mongoose';
 import config from './environment';
 import routes from './routes';
 
@@ -11,6 +12,20 @@ app.use(cors({ origin: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 routes(app);
+
+mongoose.connect(config.mongo_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+const database = mongoose.connection;
+database.once("open", async () => {
+    console.log("Connected to database");
+});
+database.on("error", () => {
+    console.log("Error connecting to database");
+});
 
 app.get('/index', (req, res) => {
     console.log("req: ", req)
